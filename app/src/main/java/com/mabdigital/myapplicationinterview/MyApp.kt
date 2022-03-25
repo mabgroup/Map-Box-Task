0bad3373076ad2e48596451ca67cdee1ec05a8fb
Ehsan Mohit <mohammad.mohit87@gmail.com>
2022-03-25 19:34:00 +0430

    Added MyApp.kt class as custom application class

---
+++ app/src/main/java/com/mabdigital/myapplicationinterview/MyApp.kt
@@ -0,0 +1,10 @@
+package com.mabdigital.myapplicationinterview
+
+import androidx.multidex.MultiDexApplication
+
+class MyApp : MultiDexApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
