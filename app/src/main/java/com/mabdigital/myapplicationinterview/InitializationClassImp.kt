b951142f750ca1cd67273a6ec06fa489ddc21036
Ehsan Mohit <mohammad.mohit87@gmail.com>
2022-03-30 13:32:13 +0430

    Add Initialize for Hawk (May be use in future)

--- app/src/main/java/com/mabdigital/myapplicationinterview/InitializationClassImp.kt
+++ app/src/main/java/com/mabdigital/myapplicationinterview/InitializationClassImp.kt
@@ -3,6 +3,7 @@ package com.mabdigital.myapplicationinterview
 import android.app.Application
 import com.mabdigital.core.base.markers.InitializationClass
 import com.mabdigital.myapplicationinterview.di.applicationModule
+import com.orhanobut.hawk.Hawk
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import timber.log.Timber
@@ -11,6 +12,11 @@ class InitializationClassImp : InitializationClass {
     override fun initTools(myApp: Application) {
         initKoin(myApp)
         initialisedTimber()
+        initialiseHawk(myApp)
+    }
+
+    private fun initialiseHawk(myApp: Application) {
+        Hawk.init(myApp).build()
     }
 
     private fun initialisedTimber() {
