f45e7f057846abc665ab1be203a89c0745dbe0e4
Ehsan_PC <mohammadehsan.m@780.team>
2022-04-08 14:01:32 +0430

    set safe args for details and load data from safe arg in details

--- offers/src/main/java/com/mabdigital/offers/domain/model/map/AnnotationPointModel.kt
+++ offers/src/main/java/com/mabdigital/offers/domain/model/map/AnnotationPointModel.kt
@@ -1,12 +1,15 @@
 package com.mabdigital.offers.domain.model.map
 
+import android.os.Parcelable
 import com.mapbox.geojson.Point
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class PointDetails(
     val point:Point,
     val pointAddress:String,
     val type: TerminalLocationTypeEnum
-)
+) : Parcelable
 
 enum class TerminalLocationTypeEnum {
     Unknown,
--- offers/src/main/java/com/mabdigital/offers/presentaton/feature/details/OfferDetailsFragment.kt
+++ offers/src/main/java/com/mabdigital/offers/presentaton/feature/details/OfferDetailsFragment.kt
@@ -5,16 +5,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.mabdigital.offers.R
 import com.mabdigital.offers.databinding.DeatilsBottomSheetBinding
 import com.mabdigital.offers.databinding.MapLoadViewBinding
+import com.mabdigital.offers.domain.feature.map.MapActionEvent
 import com.mabdigital.offers.presentaton.feature.activity.OfferShareViewModel
+import io.reactivex.disposables.CompositeDisposable
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.text.NumberFormat
 
 class OfferDetailsFragment : BottomSheetDialogFragment() {
 
     private val mViewModel by viewModel<OfferShareViewModel>()
 
+    private val args by navArgs<OfferDetailsFragmentArgs>()
+    private val compositeDisposable = CompositeDisposable()
     private var _binding: DeatilsBottomSheetBinding? = null
     private val binding get() = _binding!!
 
@@ -36,10 +43,27 @@ class OfferDetailsFragment : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupList()
+        setupView()
+    }
+
+    private fun setupView() {
+        val price = String.format(getString(R.string.template_price),NumberFormat.getIntegerInstance().format(args.price.toLong()))
+        binding.priceHolder.text = price
     }
 
     private fun setupList() {
+        binding.pointList.adapter = LocationListAdapter(args.pointlist.toList()).apply {
+            val disposable = clickSubject.subscribe {
+                mViewModel.onEvent(MapActionEvent.OnPointClicked(it))
+            }
+            compositeDisposable.add(disposable)
+        }
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.clear()
+        _binding = null
     }
 
 }
