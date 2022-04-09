703b3860863f871db0459389dffab13c6408802c
780Team-MEM <mohammadehsan.m@780.team>
2022-04-09 21:25:57 +0430

    improve received model to better action

--- core/src/main/java/com/mabdigital/core/base/model/NotificationModel.kt
+++ core/src/main/java/com/mabdigital/core/base/model/NotificationModel.kt
@@ -1,11 +1,26 @@
 package com.mabdigital.core.base.model
 
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class NotificationModel(
-    val source:String,
-    val price:String,
-    val array : List<String>
-):Parcelable
+    val title: String? = "Notification",
+    val body: String? = "Offers For You!",
+    val source: String,
+    val price: String,
+    val array: List<ExtraPoint>
+) : Parcelable
+
+@Parcelize
+data class ExtraPoint(
+    @SerializedName("pointx")
+    val Latitude: Double,
+    @SerializedName("pointy")
+    val Longitude: Double,
+    @SerializedName("address")
+    val address: String,
+    @SerializedName("type")
+    val type: String
+) : Parcelable
