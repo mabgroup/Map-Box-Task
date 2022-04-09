d3c6d5873b7076da7c98a57956e709814e2977c6
780Team-MEM <mohammadehsan.m@780.team>
2022-04-09 21:26:35 +0430

    modify message service to generate notification

--- core/src/main/java/com/mabdigital/core/base/notification/MyFirebaseMessagingService.kt
+++ core/src/main/java/com/mabdigital/core/base/notification/MyFirebaseMessagingService.kt
@@ -1,14 +1,23 @@
 package com.mabdigital.core.base.notification
 
+import android.app.PendingIntent
 import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.mabdigital.core.R
+import com.mabdigital.core.base.model.NotificationModel
 import com.mabdigital.core.presentation.feature.CoreActivity
 import com.orhanobut.hawk.Hawk
 import timber.log.Timber
+import kotlin.random.Random
 
 internal const val FIREBASE_TOKEN = "com.mabdigital.myapplicationinterview.firebase_push"
+const val CHANNEL_ID = "com.mabdigital.myapplicationinterview"
 const val NOTIFICATION_DATA = "Notification"
+
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     companion object {
@@ -31,10 +40,32 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         Timber.wtf("Notification Is Here!")
         val messageData = NotificationHelper(remoteMessage)
         val model = messageData.parseToModel()
+        makeNotification(model)
+    }
+
+    private fun makeNotification(model: NotificationModel) {
+
         val intent = Intent(this, CoreActivity::class.java).apply {
-            putExtra(NOTIFICATION_DATA,model)
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            putExtra(NOTIFICATION_DATA, model)
         }
-        startActivity(intent)
+        val pendingIntent: PendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        } else {
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        }
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_baseline_textsms_24)
+            .setContentTitle(model.title)
+            .setContentText(model.body)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(Random.nextInt(),builder.build())
+        }
+
     }
 
 
